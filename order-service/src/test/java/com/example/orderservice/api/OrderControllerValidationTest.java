3ede19b40d0648e1e2d0a7e9598c880604fe2a2c
Achraf Boumessouer <bouessouerachraf@yahoo.com>
2026-06-18 17:35:53 +0200

    Replace deprecated MockBean in order tests

--- order-service/src/test/java/com/example/orderservice/api/OrderControllerValidationTest.java
+++ order-service/src/test/java/com/example/orderservice/api/OrderControllerValidationTest.java
@@ -8,8 +8,8 @@ import com.example.orderservice.service.OrderService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(OrderController.class)
@@ -17,7 +17,7 @@ class OrderControllerValidationTest {
 
   @Autowired private MockMvc mockMvc;
 
-  @MockBean private OrderService orderService;
+  @MockitoBean private OrderService orderService;
 
   @Test
   void createOrderRejectsInvalidRequest() throws Exception {
